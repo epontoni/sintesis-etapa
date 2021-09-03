282394bfb4c1c71cffa994a20e6484b6ed14c74a
Emanuel Pontoni <pontoni.emanuel@gmail.com>
2021-09-03 13:59:34 -0300

    Clipboard síntesis de etapa

---
+++ js/index.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    var celdas = document.querySelectorAll('.celda');
+    var aux = document.querySelector("#aux");
+    
+    for (var i=0, len = celdas.length; i < len; i++){
+        celdas[i].addEventListener('click', function(e){
+            navigator.clipboard.writeText(e.target.innerHTML)
+                .then(() => {
+                    console.log("Text copied to clipboard...")
+                    e.target.classList.add('active')
+                })
+                .catch( err => {
+                    console.log('Something went wrong', err);
+                })
+        })
+    }
+    
+};
